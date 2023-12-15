251d202a5e814fa2409b77201ba86331267dcac9
barteklopatka <baczkopolter@gmail.com>
2023-12-15 12:55:50 +0100

    structs-and-interfaces: using structs and methods

--- structs-and-interfaces/structs.go
+++ structs-and-interfaces/structs.go
@@ -1,5 +1,24 @@
 package structs_and_interfaces
 
-func Perimeter(width float64, length float64) float64 {
-	return (2 * width) + (2 * length)
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Length float64
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Length + r.Width)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Length * r.Width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- structs-and-interfaces/structs_test.go
+++ structs-and-interfaces/structs_test.go
@@ -3,12 +3,36 @@ package structs_and_interfaces
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	t.Run("counting perimeter of rectangle", func(t *testing.T) {
-		got := Perimeter(10.0, 10.0)
+	t.Run("counting perimeter of a rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Perimeter()
 		want := 40.0
 
 		if got != want {
 			t.Errorf("Got %.2f, want %.2f", got, want)
+		}
+	})
+
+}
+
+func TestArea(t *testing.T) {
+	t.Run("counting area of a rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := rectangle.Area()
+		want := 72.0
+
+		if got != want {
+			t.Errorf("Got %.2f, want %.2f", got, want)
+		}
+	})
+
+	t.Run("counting area of a circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("Got %g, want %g", got, want)
 		}
 	})
 

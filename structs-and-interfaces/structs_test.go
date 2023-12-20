f1679ed71495e74679068c29e01cfafe5b0e6c0d
barteklopatka <baczkopolter@gmail.com>
2023-12-20 12:36:38 +0100

    structs-and-interfaces: improved test cases readability

--- structs-and-interfaces/structs_test.go
+++ structs-and-interfaces/structs_test.go
@@ -9,7 +9,7 @@ func TestPerimeter(t *testing.T) {
 		want := 40.0
 
 		if got != want {
-			t.Errorf("Got %.2f, want %.2f", got, want)
+			t.Errorf("Got %.2f, desiredArea %.2f", got, want)
 		}
 	})
 
@@ -17,25 +17,28 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name        string
+		shape       Shape
+		desiredArea float64
 	}{
-		{Rectangle{12.0, 6.0}, 72.0},
-		{Circle{10.0}, 314.1592653589793},
-		{Triangle{20.0, 10.0}, 100},
+		{"Rectangle", Rectangle{12.0, 6.0}, 72.0},
+		{"Circle", Circle{10.0}, 314.1592653589793},
+		{"Triangle", Triangle{20.0, 10.0}, 100},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("Got %g, want %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.desiredArea {
+				t.Errorf("Shape %#v got %g, desiredArea is %g", tt.shape, got, tt.desiredArea)
+			}
+		})
 	}
-	//checkArea := func(t testing.TB, shape Shape, want float64) {
+	//checkArea := func(t testing.TB, shape Shape, desiredArea float64) {
 	//	t.Helper()
 	//	got := shape.Area()
-	//	if got != want {
-	//		t.Errorf("Got %.2f, want %.2f", got, want)
+	//	if got != desiredArea {
+	//		t.Errorf("Got %.2f, desiredArea %.2f", got, desiredArea)
 	//	}
 	//}
 	//t.Run("counting area of a rectangle", func(t *testing.T) {

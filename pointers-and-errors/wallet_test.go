31ee459b1f47ca5373321c12222228f5055883e3
b.lopatka <b.lopatka@sportradar.com>
2025-03-28 21:33:36 +0100

    pointers-and-errors: fixed unhandled err

--- pointers-and-errors/wallet_test.go
+++ pointers-and-errors/wallet_test.go
@@ -15,7 +15,8 @@ func TestWallet(t *testing.T) {
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{Bitcoin(20)}
 
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 	t.Run("withdraw insufficient funds", func(t *testing.T) {
@@ -45,5 +46,11 @@ func assertError(t testing.TB, got, want error) {
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	if got != nil {
+		t.Fatal("got an unwanted error")
 	}
 }
